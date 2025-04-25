12aebdbc62e496a71a844522bb723fd378228003
kush-sh10 <shettykush3@gmail.com>
2025-04-25 18:29:08 +0530

    removed sudo

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,11 +2,10 @@
 
 APP_DIR="/home/ubuntu/myapp"
 
-# Change ownership of the app directory to ubuntu user
-chown -R ubuntu:ubuntu "$APP_DIR"
+
 
 # Switch to the app directory
 cd "$APP_DIR"
 
 # Run npm install as ubuntu user
-sudo -u ubuntu npm install
+npm install
