7456fda90d8a411e3cc97c2860208b62039a619d
kush-sh10 <shettykush3@gmail.com>
2025-04-25 17:11:59 +0530

    changes in script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd /home/ubuntu/myapp
-npm install
+
+APP_DIR="/home/ubuntu/myapp"
+
+# Change ownership of the app directory to ubuntu user
+chown -R ubuntu:ubuntu "$APP_DIR"
+
+# Switch to the app directory
+cd "$APP_DIR"
+
+# Run npm install as ubuntu user
+sudo -u ubuntu npm install
