45c0f6647b4f1b3da640f7c36c3ebee1c4b2ef91
kush-sh10 <shettykush3@gmail.com>
2025-04-25 17:16:39 +0530

    changes in start script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd /home/ubuntu/myapp
-npm start > app.log 2>&1 &
+
+APP_DIR="/home/ubuntu/myapp"
+
+# Change ownership just in case (optional if already done)
+chown -R ubuntu:ubuntu "$APP_DIR"
+
+# Switch to the app directory
+cd "$APP_DIR"
+
+# Start the app as ubuntu user, redirecting logs
+sudo -u ubuntu nohup npm start > app.log 2>&1 &
